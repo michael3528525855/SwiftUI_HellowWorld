128af87cd33fe8da53231f69abd3cd969b2b54f1
michael3528525855 <153228492+michael3528525855@users.noreply.github.com>
2024-07-22 17:59:57 +0300

    Add files via upload

---
+++ swifUI_hallow warld/swifUI_hallow warld/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  swifUI_hallow warld
+//
+//  Created by michael on 22/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                .font(.title)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swifUI_hallow warld/swifUI_hallow warld/swifUI_hallow_warldApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifUI_hallow_warldApp.swift
+//  swifUI_hallow warld
+//
+//  Created by michael on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct swifUI_hallow_warldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
